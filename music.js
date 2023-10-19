8232ed7d6a959fe4c913c4909761418e1123e141
quangk22 <quangcute504@gmail.com>
2023-10-19 21:22:20 +0700

    heart

--- music.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', musicPlay);
-function musicPlay() {
-    document.getElementById('audio').play();
-    document.removeEventListener('DOMContentLoaded', musicPlay);
-}
